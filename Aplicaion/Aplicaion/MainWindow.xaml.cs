600529dc3eacbf877c25ea042515e81280b1d07b
unknown <juanjomedinamusellas@gmail.com>
2019-10-05 12:24:38 +0200

    Initial commit private

--- Aplicaion/Aplicaion/MainWindow.xaml.cs
+++ Aplicaion/Aplicaion/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,65 @@ namespace Aplicaion
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rectangle[][] GraphicGrid;
         public MainWindow()
         {
+
             InitializeComponent();
+
+        }
+
+        private void SetGrid_Click(object sender, RoutedEventArgs e)
+        {
+            SetGrid.IsEnabled = false;
+            DiscardGrid.IsEnabled = true;
+            ColumnSlider.IsEnabled = false;
+            RowsSlider.IsEnabled=false;
+
+            GraphicGrid = new Rectangle[Convert.ToInt32(RowsSlider.Value)][];
+            for (int j=0;j<ColumnSlider.Value;j++)
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            for (int i = 0; i < RowsSlider.Value; i++)
+            {
+                Rectangle[] GraphicRow = new Rectangle[Convert.ToInt32(ColumnSlider.Value)];
+                grid.RowDefinitions.Add(new RowDefinition());
+                for (int j = 0; j < ColumnSlider.Value; j++)
+                {
+                    Rectangle rectangle = new Rectangle();
+                    //SolidColorBrush RedBrush = new SolidColorBrush();
+                    //RedBrush.Color = Colors.Red;
+                    //rectangle.Fill = RedBrush;
+                    Grid.SetRow(rectangle, i);
+                    Grid.SetColumn(rectangle, j);
+                    grid.Children.Add(rectangle);
+                    GraphicRow[j] = rectangle;
+                }
+                GraphicGrid[i] = GraphicRow;
+
+            }
+        }
+
+        private void DiscardGrid_Click(object sender, RoutedEventArgs e)
+        {
+            SetGrid.IsEnabled = true;
+            DiscardGrid.IsEnabled = false;
+            ColumnSlider.IsEnabled = true;
+            RowsSlider.IsEnabled = true;
+            grid.Children.Clear();
+            grid.ColumnDefinitions.Clear();
+            grid.RowDefinitions.Clear();
+        }
+
+
+
+        private void RowsSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            RowsCount.Content = RowsSlider.Value.ToString();
+        }
+
+        private void ColumnSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ColumnsCount.Content = ColumnSlider.Value.ToString();
         }
     }
 }
